2d9ee071da9912a18163085a1cbcaeecbcde87e2
r0b1n-h00d <davidkiforo@gmail.com>
2022-02-28 08:50:01 -0500

    updated

--- components/ChatMessage/index.tsx
+++ components/ChatMessage/index.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Message } from "../../types";
 import moment from "moment";
+import styles from "./style";
 
 export type ChatMessageProps = {
   message: Message;
@@ -10,11 +11,26 @@ export type ChatMessageProps = {
 export default function ChatMessage(props: ChatMessageProps) {
   const { message } = props;
 
+  const isMyMessage = () => {
+    return message.user.id === "u1";
+  };
+
   return (
-    <View>
-      <Text>{message.user.name}</Text>
-      <Text>{message.content}</Text>
-      <Text>{moment(message.createdAt).fromNow()}</Text>
+    <View style={styles.container}>
+      <View
+        style={[
+          styles.messageBox,
+          {
+            backgroundColor: isMyMessage() ? "#DCF8C5" : "white",
+            alignContent: isMyMessage() ? "flex-end" : "flex-start",
+            marginLeft: isMyMessage() ? 50 : 0,
+          },
+        ]}
+      >
+        <Text>{message.user.name}</Text>
+        <Text>{message.content}</Text>
+        <Text>{moment(message.createdAt).fromNow()}</Text>
+      </View>
     </View>
   );
 }
