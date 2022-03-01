8615f065e990bbdb7644ba90ebfba0c23652ad1a
r0b1n-h00d <davidkiforo@gmail.com>
2022-03-01 13:37:34 -0500

    created contacts screen to display available contacts

---
+++ screens/Contacts.tsx
@@ -0,0 +1,29 @@
+import { FlatList, StyleSheet } from "react-native";
+import ChatListItem from "../components/ChatListItem";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+
+import chatRooms from "../data/ChatRooms";
+import NewMessageButton from "../components/NewMessageButton";
+
+export default function Contacts({ navigation }: RootTabScreenProps<"TabOne">) {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={{ width: "100%" }}
+        data={chatRooms}
+        renderItem={({ item }) => <ChatListItem chatRoom={item} />}
+        keyExtractor={(item) => item.id}
+      />
+      <NewMessageButton />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
